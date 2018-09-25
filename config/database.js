23271d7d52d1a6e9dc94d49700f1092d16a9cf83
XONtech <xontech2k18@gmail.com>
2018-09-25 07:21:26 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 // config/database.js
 module.exports = {
-    'url' : 'mongodb://localhost:27017/TracFlix'
+    // 'url' : 'mongodb://localhost:27017/TracFlix'
+    url : "mongodb://admin:Sicar1996@ds137611.mlab.com:37611/tracflix"
 };
